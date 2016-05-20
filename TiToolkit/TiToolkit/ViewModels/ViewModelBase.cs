b7aa6834ad272e5a16a9e9c0fc6ed89b7909a0d1
Mark Tinderholt <markti@outlook.com>
2016-05-20 15:15:01 -0400

    added view model base and OC<T> AddRange extension method

---
+++ TiToolkit/TiToolkit/ViewModels/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiToolkit.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Fires an event when called. Used to update the UI in the MVVM world.
+        /// [CallerMemberName] Ensures only the peoperty that calls it gets the event
+        /// and not every property
+        /// </summary>
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}

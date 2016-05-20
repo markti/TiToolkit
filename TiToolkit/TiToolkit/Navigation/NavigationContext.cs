c6d7c43de1dce6c2fc9f72fc26db87eb06eb0ce7
Mark Tinderholt <markti@outlook.com>
2016-05-20 14:55:07 -0400

    navigation framework

---
+++ TiToolkit/TiToolkit/Navigation/NavigationContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TiToolkit.Navigation
+{
+    public static class NavigationContext
+    {
+        private static MasterDetailPage _shell;
+
+        public static void Initialize(MasterDetailPage shell)
+        {
+            _shell = shell;
+        }
+
+        public static void NavigateTo(Type pageType, Dictionary<string, object> navigationParameters)
+        {
+            var page = Activator.CreateInstance(pageType) as Page;
+            var navigationPage = page as INavigationPage;
+            if (navigationPage != null)
+            {
+                navigationPage.OnNavigatedTo(navigationParameters);
+            }
+            _shell.Detail = page;
+        }
+    }
+}
